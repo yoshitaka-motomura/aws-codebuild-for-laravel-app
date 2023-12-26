94e3c92562c80c7e839046c29814315a93428305
Yoshitaka motomura <stay.daybreak@gmail.com>
2023-12-27 02:47:28 +0900

    Update asset links to use secure_asset() function

--- service/resources/views/layouts/app.blade.php
+++ service/resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('/css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/css/reset.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}">
 </head>
 
 <body>
