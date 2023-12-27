06e3ab6af36459c81887c79e789fdc63faaceed2
Yoshitaka motomura <stay.daybreak@gmail.com>
2023-12-28 01:35:03 +0900

    Add new files and configurations for the web application

---
+++ web/src/views/DemoView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>My Demo View</h1>
+    <p>My demo view is working!</p>
+    <p>{{ state.message }}</p>
+  </div>
+</template>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+const state = reactive({
+  message: 'hello world'
+})
+</script>
